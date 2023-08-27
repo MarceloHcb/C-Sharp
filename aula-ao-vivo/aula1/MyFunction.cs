8046b9bd1c119c6fb63beb0c83bd9c5c4535201e
MarceloHcb <marcelohcb@msn.com>
2023-08-26 21:49:06 -0300

    up

---
+++ aula-ao-vivo/aula1/MyFunction.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MainNamespace
+{
+    public class MyFunction
+    {
+        public static string GetHelloMessage()
+        {
+            return "Iniciando a Aula ao Vivo!!";
+        }
+    }
+}
---
+++ aula-ao-vivo/aula1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MainNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Aula ao vivo!");
+
+            string message = MyFunction.GetHelloMessage();
+            Console.WriteLine(message);
+        }
+    }
+}
